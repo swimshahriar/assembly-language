504e11ad4471e1c5892cceeb14ea2b0881fb99ad
Shahriar Swim <shahriar2k18@gmail.com>
2021-03-13 19:54:54 +0600

    lab01

---
+++ lab01.asm
@@ -0,0 +1,17 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+; add your code here 
+
+    mov ax, 2
+    add bx, 2
+    mov ax, bx
+
+ret
+
+
+
+
---
+++ mycode.asm
@@ -0,0 +1,17 @@
+.MODEL SMALL
+.STACK 200H
+.DATA
+    ;WHERE ALL THE VAIRABLES WILL BE DECLARED
+    
+.CODE
+    ;CODE SEGMENT
+    MAIN PROC
+        ;STATEMENT
+        
+        
+        MOV AH , 4CH
+        INT 21H
+    MAIN ENDP
+    
+    
+END MAIN
