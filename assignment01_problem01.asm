2fd61dbd58cd36a4f276bb3a17dd58d11ec57b35
Shahriar Swim <Shahriar2k18@gmail.com>
2021-04-20 23:36:36 +0600

    assignment01_problem01

--- assignment01_problem01.asm
+++ assignment01_problem01.asm
@@ -1,22 +1,28 @@
+include 'emu8086.inc'
+
 .model small
 .stack 100h
 
 .data
-
 
 .code
 main proc
     mov ax, @data
     mov ds, ax
     
-        mov ah, 01h
-        int 21h
-        
-        mov cl, 01h
-        add cl, al
-        
-        
-        
+    mov cl, 00100001b
+   
+   shl cl, 1
+   
+   jc negative
+   print 'positive'
+   jmp break
+   
+   negative:
+       print 'negative'
+       
+       
+   break:
    
 endp main
 end main
